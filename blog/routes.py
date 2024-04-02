190f3ed6aa6a6d2e54d96b4fd0df8d2b547f9792
Sebastian Slusarczyk. <sslusarczyk87@gmail.com.~>
2024-04-02 11:36:29 -0400

    Zadanie Modul 16.3

--- blog/routes.py
+++ blog/routes.py
@@ -1,11 +1,21 @@
 
 # blog/routes.py
 
-from flask import render_template, request, redirect, flash
+from flask import render_template, request,session, redirect, flash, url_for
 from blog import app
 from blog.models import Entry, db
-from blog.forms import EntryForm
+from blog.forms import EntryForm, LoginForm
+import functools
 
+
+def login_required(view_func):
+   @functools.wraps(view_func)
+   def check_permissions(*args, **kwargs):
+       if session.get('logged_in'):
+           return view_func(*args, **kwargs)
+       return redirect(url_for('login', next=request.path))
+   return check_permissions
+   
 
 @app.route("/")
 def index():
@@ -17,6 +27,7 @@ def index():
 
 @app.route("/new-post/", methods=["GET", "POST"])
 @app.route("/edit-post/<int:entry_id>", methods=["GET", "POST"])
+@login_required
 def manage_entry(entry_id=None):
     entry = Entry.query.get_or_404(entry_id) if entry_id else None
     form = EntryForm(obj=entry) if entry else EntryForm()
@@ -41,5 +52,42 @@ def manage_entry(entry_id=None):
 
     return render_template("entry_form.html", form=form, errors=errors)
    
-   
+@app.route("/login/", methods=['GET', 'POST'])
+def login():
+   form = LoginForm()
+   errors = None
+   next_url = request.args.get('next')
+   if request.method == 'POST':
+       if form.validate_on_submit():
+           session['logged_in'] = True
+           session.permanent = True  # Use cookie to store session.
+           flash('You are now logged in.', 'success')
+           return redirect(next_url or url_for('index'))
+       else:
+           errors = form.errors
+   return render_template("login_form.html", form=form, errors=errors)
+
+
+@app.route('/logout/', methods=['GET', 'POST'])
+def logout():
+   if request.method == 'POST':
+       session.clear()
+       flash('You are now logged out.', 'success')
+   return redirect(url_for('index'))
  
+ 
+
+@app.route("/drafts/", methods=['GET'])
+@login_required
+def list_drafts():
+   drafts = Entry.query.filter_by(is_published=False).order_by(Entry.pub_date.desc())
+   return render_template("drafts.html", drafts=drafts)
+   
+   
+@app.route("/delete-post/<int:entry_id>", methods=["POST"])
+def delete_entry(entry_id):
+    entry = Entry.query.get_or_404(entry_id)
+    db.session.delete(entry)
+    db.session.commit()
+    flash('Wpis został usunięty pomyślnie!', 'success')
+    return redirect(url_for('index'))
